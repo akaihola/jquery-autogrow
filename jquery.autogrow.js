7d6603ef80fbaa3e083834a7906aee556d7cb132
Stefan Heinemann <sh@adfinis.com>
2012-01-06 09:41:56 +0100

    Some code formatting changes
    * More or less comply with the 80 characters per line restriction
    * Used spaces for the alignment of variable assignments

--- jquery.autogrow.js
+++ jquery.autogrow.js
@@ -39,15 +39,15 @@
 
 	jQuery.autogrow = function (e, o)
 	{
-		this.options		  	  = o || {};
-		this.dummy			  	  = null;
-		this.interval	 	  	  = null;
-		this.line_height	  	  = this.options.lineHeight || parseInt(jQuery(e).css('line-height'));
-		this.min_height		  	  = parseInt(this.options.minHeight || jQuery(e).css('min-height'));
-		this.max_height		  	  = this.options.maxHeight || parseInt(jQuery(e).css('max-height'));;
-		this.expand_callback	  = this.options.expandCallback;
+		this.options              = o || {};
+		this.dummy                = null;
+		this.interval             = null;
+		this.line_height          = this.options.lineHeight || parseInt(jQuery(e).css('line-height'));
+		this.min_height           = parseInt(this.options.minHeight || jQuery(e).css('min-height'));
+		this.max_height           = this.options.maxHeight || parseInt(jQuery(e).css('max-height'));;
+		this.expand_callback      = this.options.expandCallback;
 		this.resize_callback      = this.options.resizeCallback || function() {};
-		this.textarea		  	  = jQuery(e);
+		this.textarea             = jQuery(e);
 		this.even_numbered_height = this.options.evenNumberedHeight || false;
 
 		if(this.line_height == NaN)
@@ -58,23 +58,37 @@
 	};
 
 	jQuery.autogrow.fn = jQuery.autogrow.prototype = {
-    autogrow: '1.2.2'
-  };
+		autogrow: '1.2.2'
+	};
 
  	jQuery.autogrow.fn.extend = jQuery.autogrow.extend = jQuery.extend;
 
 	jQuery.autogrow.fn.extend({
-
 		init: function() {
 			var self = this;
 			this.textarea.css({overflow: 'hidden', display: 'block'});
-			this.textarea.bind('focus', function() { self.startExpand() } ).bind('blur', function() { self.stopExpand() });
+			this.textarea.bind(
+				'focus', 
+				function() {
+					self.startExpand() 
+				}
+			).bind(
+				'blur',
+				function() {
+					self.stopExpand() 
+				}
+			);
 			this.checkExpand();
 		},
 
 		startExpand: function() {
-		  var self = this;
-			this.interval = window.setInterval(function() {self.checkExpand()}, 400);
+			var self = this;
+			this.interval = window.setInterval(
+				function() {
+					self.checkExpand()
+				},
+				400
+			);
 		},
 
 		stopExpand: function() {
@@ -82,21 +96,20 @@
 		},
 
 		checkExpand: function() {
-
 			if (this.dummy == null)
 			{
 				this.dummy = jQuery('<div></div>');
 				this.dummy.css({
-												'font-size'  : this.textarea.css('font-size'),
-												'font-family': this.textarea.css('font-family'),
-												'width'      : this.textarea.css('width'),
-												'padding'    : this.textarea.css('padding'),
-												'line-height': this.line_height + 'px',
-												'overflow-x' : 'hidden',
-												'position'   : 'absolute',
-												'top'        : 0,
-												'left'		 : -9999
-												}).appendTo('body');
+					'font-size'  : this.textarea.css('font-size'),
+					'font-family': this.textarea.css('font-family'),
+					'width'      : this.textarea.css('width'),
+					'padding'    : this.textarea.css('padding'),
+					'line-height': this.line_height + 'px',
+					'overflow-x' : 'hidden',
+					'position'   : 'absolute',
+					'top'        : 0,
+					'left'       : -9999
+					}).appendTo('body');
 			}
 
 			// Strip HTML tags
@@ -116,14 +129,18 @@
 			{
 				this.dummy.html(html);
 
-				if (this.max_height > 0 && (this.dummy.height() + this.line_height > this.max_height))
+				if  (   this.max_height > 0
+						&& (this.dummy.height() + this.line_height > this.max_height)
+					)
 				{
 					this.textarea.css('overflow-y', 'auto');
 				}
 				else
 				{
 					this.textarea.css('overflow-y', 'hidden');
-					if (this.textarea.height() < this.dummy.height() + this.line_height || (this.dummy.height() < this.textarea.height()))
+					if  (   this.textarea.height() < this.dummy.height() + this.line_height
+							|| (this.dummy.height() < this.textarea.height())
+						)
 					{
 						var newHeight = (this.dummy.height() + this.line_height);
 						if (newHeight < this.min_height) 
@@ -140,9 +157,15 @@
 				}
 			}
 
-			if (this.expand_callback) {
+			if (this.expand_callback) 
+			{
 				var self = this;
-				window.setTimeout(function(){self.expand_callback()},500);
+				window.setTimeout(
+					function() {
+						self.expand_callback()
+					},
+					500
+				);
 			}
 		}
 
